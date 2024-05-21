451fdb8f2ef50a82e8d4c43814167cfaecabd5c8
eunseo <dmatj4180@gmail.com>
2024-05-21 15:10:19 +0900

    hhhh

--- src/main/frontend/src/component/nav/Recruiting.jsx
+++ src/main/frontend/src/component/nav/Recruiting.jsx
@@ -7,34 +7,36 @@ import './Recruiting.css';
 
 function Recruiting() {
   return (
-    <div>
-      <div className="cmon">
-        <div className="newmember">
-          <p>✈️채용 일정</p>
+      <div>
+        <div className="recruiting-container">
+          <div className="cmon">
+            <div className="newmember">
+              <p>✈️채용 일정</p>
+            </div>
+          </div>
+          <div className="recruit_posting">
+            <nav className="recruit_nav">
+              <ul className="recruit_nav-links">
+                <li>
+                  <Link to="/cabin">캐빈 승무원</Link>
+                </li>
+                <li>
+                  <Link to="/papago">기내 통역원</Link>
+                </li>
+                <li>
+                  <Link to="/intern">인턴 승무원</Link>
+                </li>
+              </ul>
+            </nav>
+            {/* Route 설정을 nav 바로 아래에 배치하여 선택된 링크에 따라 컨텐츠가 변경되도록 합니다. */}
+            <Routes>
+              <Route path="/cabin" element={<Cabin />} />
+              <Route path="/papago" element={<Papago />} />
+              <Route path="/intern" element={<Intern />} />
+            </Routes>
+          </div>
         </div>
       </div>
-      <div className="recruit_posting">
-        <nav className="recruit_nav">
-          <ul className="recruit_nav-links">
-            <li>
-              <Link to="/cabin">캐빈 승무원</Link>
-            </li>
-            <li>
-              <Link to="/papago">기내 통역원</Link>
-            </li>
-            <li>
-              <Link to="/intern">인턴 승무원</Link>
-            </li>
-          </ul>
-        </nav>
-        {/* Route 설정을 nav 바로 아래에 배치하여 선택된 링크에 따라 컨텐츠가 변경되도록 합니다. */}
-        <Routes>
-          <Route path="/cabin" element={<Cabin />} />
-          <Route path="/papago" element={<Papago />} />
-          <Route path="/intern" element={<Intern />} />
-        </Routes>
-      </div>
-    </div>
   );
 }
 
